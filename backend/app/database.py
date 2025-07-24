4da2f02cf3013f48718f042ae3e431f936dc8dbc
Jean-Baptiste Renault <81814445+bantra7@users.noreply.github.com>
2025-07-24 10:20:01 +0200

    Update database.py

--- backend/app/database.py
+++ backend/app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "duckdb:///workspace/data/boardgames.db"
+SQLALCHEMY_DATABASE_URL = "duckdb:///mnt/data/boardgames.db"
 
 
 engine = create_engine(
